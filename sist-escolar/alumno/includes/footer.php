fcb5a087c0e09d8b6a4786e5b4416d64683262bf
David Tomala <david.tomalab@ug.edu.ec>
2021-09-03 12:50:04 -0500

    Modulos y Roles

---
+++ sist-escolar/alumno/includes/footer.php
@@ -0,0 +1,16 @@
+   
+   <!-- Essential javascripts for application to work-->
+   <script src="../js/jquery-3.5.1.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/main.js"></script>
+    <script src="../js/fontawesome.js"></script>
+    <script type="text/javascript" src="../js/plugins/sweetalert.min.js"></script>
+    <!-- The javascript plugin to display page loading on top-->
+    <script type="text/javascript" src="../js/plugins/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="../js/plugins/dataTables.bootstrap.min.js"></script>
+   
+    <script src="../js/plugins/pace.min.js"></script>
+    <script type="text/javascript" src="js/functions-entrega.js"></script>
+    </body>
+ </html>
